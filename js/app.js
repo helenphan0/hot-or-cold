3e0842351e5b09a83c7ca10394301def75627460
helenphan0 <helen.phan@gmail.com>
2016-07-23 15:04:03 -0400

    V2 no repeat guesses

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 
 	var gameNumber = Math.floor(Math.random() * (100 - 1 + 1)) + 1;
-
+	var guessArr = [];
 
 	/*--- Display information modal box ---*/
   	$(".what").click(function(){
@@ -20,54 +20,63 @@ $(document).ready(function(){
 		var x = $('#userGuess').val();
 		if (x<1 || x>100 || parseInt(x) != x) {
 			alert ("Guess a number from 1 to 100")
-				return false;}
+			return false;
+		};
 
-	/*--- Check if Number has been Guessed ---*/
-		if ( x == $(this).parent().find('li:contains(x)')) {
-			alert ("Number already guessed");
-				return false;};
-
+	/*--- List guessed numbers, Prevent repeat guesses  ---*/
+		var i = '<li>'+x+'</li>';
+		if ( guessArr.indexOf(i) == -1) {
+			guessArr.push(i);
+			$(this).parent().find('#guessList').html(guessArr);
+		}
+		else	{
+			alert ('Number guessed already');
+			return false;
+		};
 
 	/*--- Counting the number of guesses ---*/
-		var guessCount = parseInt($('#count').html());
-		var guessCount = guessCount + 1;
+		var guessCount = parseInt($('#count').html()) + 1;
 		$(this).parent().find('#count').text(guessCount);
 
 
 	/*--- Guesses and Feedback ---*/
 		var diff = Math.abs(gameNumber - x);
 			if (diff == 0) {
-				$(this).parent().find('#feedback').html('Ding ding!');}
+				$(this).parent().find('#feedback').html('Ding ding!');
+			}
 
 			else if (diff <= 10) {
-				$(this).parent().find('#feedback').html('Very Hot');}
+				$(this).parent().find('#feedback').html('Very Hot');
+			}
 
 			else if (diff > 10 && diff <= 20) {
-				$(this).parent().find('#feedback').html('Hot');}
+				$(this).parent().find('#feedback').html('Hot');
+			}
 
 			else if (diff > 20 && diff <= 30) {
-				$(this).parent().find('#feedback').html('Warmer');}
+				$(this).parent().find('#feedback').html('Warmer');
+			}
 
 			else if (diff > 30 && diff <= 45) {
-				$(this).parent().find('#feedback').html('Cooler');}
+				$(this).parent().find('#feedback').html('Cooler');
+			}
 
 			else if (diff > 45 && diff <= 60) {
-				$(this).parent().find('#feedback').html('Cold');}
+				$(this).parent().find('#feedback').html('Cold');
+			}
 
 			else if (diff > 60 && diff <= 99) {
-				$(this).parent().find('#feedback').html('Very Cold');}
-
-	/*--- List Guessed Numbers ---*/
-		$(this).parent().find('#guessList').append('<li>' + x + '</li>');
-
+				$(this).parent().find('#feedback').html('Very Cold');
+			}
 
 		event.preventDefault();
 		$('#userGuess').val('');
+
 	});
 
-
 	$('body').on('click', '.new', function() {
-		location.reload();});
+		location.reload();
+	});
 
 })
 
